66b77065c4d8c8d600f9de13284c14d08febcaa8
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-06-30 11:04:12 +0200

    Fjerner loggetest igjen

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,7 +1,3 @@
-import {LoggerService, SecureLoggerService} from "@bidrag/common";
-
 export default function Index() {
-    SecureLoggerService.info("Sikker hei fra index")
-    LoggerService.info("Bare hei fra index")
     return <div>Hallo hallo</div>;
 }
