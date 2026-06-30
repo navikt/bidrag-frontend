6fdfca93cc90163c7f6706c35dfd6003f4a63934
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-06-30 10:53:36 +0200

    Tester logging

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,4 +1,7 @@
-export default function Index() {
+import {LoggerService, SecureLoggerService} from "@bidrag/common";
 
+export default function Index() {
+    SecureLoggerService.info("Sikker hei fra index")
+    LoggerService.info("Bare hei fra index")
     return <div>Hallo hallo</div>;
 }
