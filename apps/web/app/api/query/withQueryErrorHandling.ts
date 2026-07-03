ccf623dffb8a86fc43ab9a949cb58bdfc3d4aaad
Håvard Nybøe <40893697+havardnyboe@users.noreply.github.com>
2026-07-03 13:29:31 +0200

    Send tom liste når transaksjoner ikke finnes for reskontro (#21)

--- apps/web/app/api/query/withQueryErrorHandling.ts
+++ apps/web/app/api/query/withQueryErrorHandling.ts
@@ -1,14 +1,12 @@
-import { SecureLoggerService } from "@bidrag/common";
 import { type ProblemDetail, TilgangsFeilError } from "@bidrag/api";
-import axios, { AxiosError } from "axios";
-
-
-
+import { ApiError, SecureLoggerService } from "@bidrag/common";
+import axios, { type AxiosError } from "axios";
 
 export async function withQueryErrorHandling<T>(
     queryName: string,
     fn: () => Promise<T>,
-    context: Record<string, string | number | undefined> = {}
+    context: Record<string, string | number | undefined> = {},
+    notFoundValue?: T,
 ): Promise<T> {
     try {
         return await fn();
@@ -17,13 +15,27 @@ export async function withQueryErrorHandling<T>(
 
         const status = axiosError?.response?.status;
         if (status === 403 || status === 401) {
-            await SecureLoggerService.warn(`Ingen tilgang til ${queryName} for ${context}`);
-            throw new TilgangsFeilError(`Du har ikke tilgang til ${queryName} for ${context}`);
+            await SecureLoggerService.warn(
+                `Ingen tilgang til ${queryName} for ${context}`,
+            );
+            throw new TilgangsFeilError(
+                `Du har ikke tilgang til ${queryName} for ${context}`,
+            );
+        }
+        if (status === 404 && notFoundValue !== undefined) {
+            return notFoundValue;
         }
         // Sjekk om det er en ProblemDetail
         if (axios.isAxiosError<ProblemDetail>(error)) {
             if (error.response) {
-                throw error.response.data;
+                const problemDetail = error.response.data;
+                throw new ApiError(
+                    problemDetail?.detail ?? `Feil ved kall til ${queryName}`,
+                    error.stack ?? "",
+                    undefined,
+                    problemDetail?.status ?? error.response.status,
+                    error,
+                );
             }
         }
 
