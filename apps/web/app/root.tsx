3a7c49c56c76f90f142fe99fbc1f8e0a0d36c023
Hans-Petter Vadseth <hans-petter.vadseth@nav.no>
2026-07-03 11:48:54 +0200

    sladding (#20)
    * Importerer css for sladding

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -9,9 +9,9 @@ import "./index.css";
 import { Loader } from "@navikt/ds-react";
 import { bisysParamsMiddleware } from "~/common/bisys/bisys-params.middleware.ts";
 import { ClientOnly } from "~/common/ClientOnly.tsx";
+import ErrorPage from "~/common/components/errorpage/ErrorPage.tsx";
 import { AppLayout } from "~/common/header/AppLayout.tsx";
 import type { Route } from "./+types/root.ts";
-import ErrorPage from "~/common/components/errorpage/ErrorPage.tsx";
 
 export const middleware = [authMiddleware];
 export const clientMiddleware = [bisysParamsMiddleware];
@@ -32,7 +32,9 @@ export default function App({ loaderData }: Route.ComponentProps) {
 
     return (
         <QueryClientWrapper>
-            <FaroErrorBoundary fallback={(error) => <RootErrorBoundary error={error} />}>
+            <FaroErrorBoundary
+                fallback={(error) => <RootErrorBoundary error={error} />}
+            >
                 <AppLayout bruker={navUser}>
                     <ClientOnly fallback={<Loader size="large" />}>
                         <Outlet />
@@ -47,8 +49,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="nb">
             <head>
+                <title>Bidrag</title>
                 <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
                 <Meta />
                 <Links />
             </head>
