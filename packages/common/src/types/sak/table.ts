5e89cfe71380114f7a7776f8690d3c4dbca05964
Bjørn Erik Sannes <bjorn.erik.sannes@nav.no>
2026-07-16 09:49:28 +0200

    feat(common): add sak domain types
    Moves shared domain types from bidrag-ui/sak-ui into packages/common
    so they can be consumed by packages/utils and eventually replace
    local copies in bidrag-ui.
    
    - rolleveileder.ts: Sakskategori, Rolleveileder
    - personsearch.ts: FamileenhetDto, RelasjonType, ForelderrolleType
    - table.ts: IFamilierelasjonTableData, IBarnRolleTableData,
      IOpprettSakRequest, IOpprettSakRolleDto
    - person.ts: IPerson, IPersonensReellMottakerRolle

---
+++ packages/common/src/types/sak/table.ts
@@ -0,0 +1,24 @@
+import { RelasjonType } from "./personsearch";
+import { RolleTypeAbbreviation } from "../roller/RolleType";
+
+export interface IOpprettSakRolleDto {
+    fodselsnummer?: string;
+    type: RolleTypeAbbreviation | "BA" | "BM" | "BP" | "FR" | "RM";
+    rolleType?: RolleTypeAbbreviation | "BA" | "BM" | "BP" | "FR" | "RM";
+    reellMottager?: string | null;
+}
+
+export interface IOpprettSakRequest {
+    eierfogd: string;
+    roller: IOpprettSakRolleDto[];
+}
+
+    rolle: RolleTypeAbbreviation;
+    relasjon: RelasjonType;
+    fodselsnummer: string;
+    navn: string;
+    visningsnavn: string;
+    isChecked: boolean;
+}
+
+export type IBarnRolleTableData = Omit<IFamilierelasjonTableData, "isChecked">;
