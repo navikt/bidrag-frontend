f283b4d2f36a226f587ad929f5a26eaa2ebe25be
Ugur Alpay Cenar <cenar93@gmail.com>
2026-08-21 11:37:16 +0200

    Oppdatere behandling app

--- apps/behandling-app/src/bootstrap.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import { initMock } from "./__mocks__/msw";
-import App from "./app";
-
-// This file is only used for development. The entrypoint is under pages folder
-initMock();
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
