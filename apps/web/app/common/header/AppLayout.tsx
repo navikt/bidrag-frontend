791cf1cfedf3cef3169d44758e7c3183aa41eea7
Ugur Alpay Cenar <cenar93@gmail.com>
2026-08-25 10:16:37 +0200

    Diverse fiks for behandling-app

--- apps/web/app/common/header/AppLayout.tsx
+++ apps/web/app/common/header/AppLayout.tsx
@@ -12,9 +12,7 @@ export function AppLayout({ children, bruker, bisysUrl }: AppLayoutProps) {
     return (
         <Page>
             <AppHeader bruker={bruker ?? undefined} bisysUrl={bisysUrl} />
-            <Page.Block as="main">
-                {children}
-            </Page.Block>
+            <Page.Block as="main">{children}</Page.Block>
         </Page>
     );
 }
