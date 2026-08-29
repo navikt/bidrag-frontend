cf16894332ab96aae0c94f343a63d2d44d211db8
Ugur Alpay Cenar <cenar93@gmail.com>
2026-08-29 18:04:34 +0200

    Fikset feil i bygg og migrering

--- packages/common/src/react_components/dokument/AapneDokumentKnapp.tsx
+++ packages/common/src/react_components/dokument/AapneDokumentKnapp.tsx
@@ -131,7 +131,6 @@ export default function AapneDokumentKnapp({
     return (
         <>
             {renderÅpneknapp()}
-            {variant === "ikon" && kanÅpnesMedMbdok && laster && <Loader size="xsmall" title="Åpner dokument …" />}
             {visRedigering && (
                 <Button
                     size="small"
