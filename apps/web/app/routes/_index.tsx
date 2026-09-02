89315a6f52d61d9a69cf02a1a689643f6ae92d28
Ugur Alpay Cenar <cenar93@gmail.com>
2026-09-02 15:06:18 +0200

    Endre VedtakWrapper til å vise feilmeldinger grupper på saksnummer

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -2,5 +2,5 @@ import { redirect } from "react-router";
 import { env } from "~/env.server.ts";
 
 export async function loader() {
-    return redirect(env.BISYS_URL);
+    return redirect(env.BISYS_URL, 307);
 }
