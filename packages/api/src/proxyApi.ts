ab2d2dce7553d36d9b11604e2f9b7ea6eb659c90
Ugur Alpay Cenar <cenar93@gmail.com>
2026-09-22 09:45:10 +0200

    Hent feilmelding fra Warning header i respons for visning av feilmelding

--- packages/api/src/proxyApi.ts
+++ packages/api/src/proxyApi.ts
@@ -1,4 +1,4 @@
-import type { AxiosInstance } from "axios";
+import type {AxiosError, AxiosInstance} from "axios";
 
 interface AxiosClient {
     instance: AxiosInstance;
@@ -10,5 +10,14 @@ interface UseApiOptions {
 
 export function proxy<T extends AxiosClient>(api: T, options: UseApiOptions): T {
     api.instance.defaults.baseURL = `/proxy/${options.app}`;
+    api.instance.interceptors.response.use(
+        (response) => response,
+        async (error: AxiosError) => {
+            const headers = error.response?.headers;
+            const warningMessage = headers?.["warning"];
+            error.message = warningMessage ? warningMessage : error.message;
+            return Promise.reject(error);
+        },
+    );
     return api;
 }
