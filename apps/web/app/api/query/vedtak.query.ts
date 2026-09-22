6d29ab18c4d6d64cf18fc2ec08387350587f048d
Ugur Alpay Cenar <cenar93@gmail.com>
2026-09-22 09:59:08 +0200

    Hent feilmelding fra Warning header i respons for visning av feilmelding

--- apps/web/app/api/query/vedtak.query.ts
+++ apps/web/app/api/query/vedtak.query.ts
@@ -1,6 +1,7 @@
 import { BIDRAG_VEDTAK_API } from "@bidrag/api";
 import { SecureLoggerService, withQueryErrorHandling } from "@bidrag/common";
 import { queryOptions } from "@tanstack/react-query";
+import {AxiosError} from "axios";
 
 export function hentVedtakQuery(vedtaksId: number) {
     return queryOptions({
@@ -10,9 +11,16 @@ export function hentVedtakQuery(vedtaksId: number) {
                 "hent_vedtak",
                 async () => {
                     if (!vedtaksId) throw new Error("Vedtaksid is required");
-                    const { data } = await BIDRAG_VEDTAK_API.vedtak.hentVedtak(vedtaksId);
-                    await SecureLoggerService.info(`Hentet vedtak  ${vedtaksId}`);
-                    return data;
+                    try {
+                        const { data } = await BIDRAG_VEDTAK_API.vedtak.hentVedtak(vedtaksId);
+                        await SecureLoggerService.info(`Hentet vedtak  ${vedtaksId}`);
+                        return data;
+                    // biome-ignore lint/suspicious/noExplicitAny: Ignorerer for å logge feil
+                    } catch (error: any) {
+                        await SecureLoggerService.error(`Feil ved henting av vedtak  ${vedtaksId}`, error);
+                        return null
+                    }
+
                 },
                 { vedtaksId },
             ),
